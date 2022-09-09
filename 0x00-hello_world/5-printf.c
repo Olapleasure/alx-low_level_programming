83b23978b9d2299285c92ae4f34f891086ad08af
olatunji Olawumi <olawunmiabike5@gmail.com>
2022-09-08 17:52:06 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+
+/**
+* main - prints the string in the put function
+*
+*Description: using te main function
+*his program prints "Programming is like building a multilingual puzzle 
+*Return: 0
+*/
+int main(void
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
