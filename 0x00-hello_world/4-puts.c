7342bceaf8eafe646da79f91b9e2f5ef98391350
olatunji Olawumi <olawunmiabike5@gmail.com>
2022-09-08 17:21:39 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
+
 /**
-
-* main - prints the phrase "Programming is like building a multilingual puzzle
-
-* Return: 0 if exited properly, non-zero otherwise
-
+* main - prints the string in the put function
+*
+*Description: using te main function
+*this program prints "Programming is like building a multilingual puzzle
+*Return: 0
 */
-
 int main(void)
-
 {
-
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
-
 }
